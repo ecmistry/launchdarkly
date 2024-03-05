e27c7a84afb5a5c855158fbd5b6f02bc4254b4ce
ecmistry <eleshmistry@gmail.com>
2024-03-05 17:56:33 +0000

    Updated Committ

--- src/ldClient.js
+++ src/ldClient.js
@@ -1,7 +1,7 @@
 import { initialize } from 'launchdarkly-react-client-sdk';
 
 const ldClient = initialize('65e717009460c60f8dce3457', {
-  key: 'sdk-8927c898-c800-4d1f-b27d-9b4d1094a6a7',
+  key: '',
   name: 'eleshmistry@gmail.com',
 });
 
